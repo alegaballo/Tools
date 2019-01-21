a0f3627f8439ca33c1193e7c587184b693ecc653
Matteo Romiti <matt.7.19.93@gmail.com>
2019-01-21 11:09:05 +0100

    add nutcracker and sort_tracks

---
+++ playlist_creator/sort_tracks.py
@@ -0,0 +1,14 @@
+# import re
+import pandas as pd
+# import numpy as np
+# from collections import Counter
+# import argparse
+# import os.path
+# import time
+# from datetime import datetime
+
+tracks = pd.read_csv('sorted_tracks.csv', dtype={'tags': str, 'genre': str})
+tracks.artists = tracks.artists.str.lower()
+tracks.title = tracks.title.str.lower()
+tracks = tracks.sort_values(['genre', 'artists', 'tags','title'])
+tracks.to_csv('sorted_tracks.csv', index=False)
